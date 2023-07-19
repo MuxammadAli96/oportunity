1950d779aa51ca56ffde36edd75165a8e2e2c776
Maxmud <maxmudov0696@mail.com>
2023-07-19 09:17:24 +0500

    lesson20

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", ' ');
- alert ("Вы посмотрели " +  numberOfFilms + " фильма");
+// let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", ' ');
+//  alert ("Вы посмотрели " +  numberOfFilms + " фильма");
 
-let personalMovieDB ={
-    count:numberOfFilms ,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const a = prompt ("Один из последних просмотренных фильмов ?", ''),
-      b= prompt ("На сколько оцените его ?" , ''),
-      c = prompt ("Один из последних просмотренных фильмов ?", ''),
-      d= prompt ("На сколько оцените его ?" , '');
+// let personalMovieDB ={
+//     count:numberOfFilms ,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+// const a = prompt ("Один из последних просмотренных фильмов ?", ''),
+//       b= prompt ("На сколько оцените его ?" , ''),
+//       c = prompt ("Один из последних просмотренных фильмов ?", ''),
+//       d= prompt ("На сколько оцените его ?" , '');
 
 
-personalMovieDB.movies[a]= b;
-personalMovieDB.movies[c]= d;
+// personalMovieDB.movies[a]= b;
+// personalMovieDB.movies[c]= d;
 
-console.log(personalMovieDB);
-console.log (numberOfFilms);
+// console.log(personalMovieDB);
+// console.log (numberOfFilms);
+
+// if (0){
+//     console.log("OK")
+// }
+// else{
+//     console.log ("not ok")
+// }
+
+// const num = 10;
+// (num===10) ? console.log('yahu') : console.log('hup'); 
+// const num =5 ;
+// switch (num){
+//     case 1:
+//     console.log("NO");
+//     break;
+//     case 2:
+//     console.log("NO");
+//     break;
+//     case 3:
+//     console.log("NO");
+//     break;
+//     case 4:
+//     console.log("NO");
+//     break;
+//     case 5:
+//     console.log("ok");
+//     break;
+//     default :
+//     console.log ("нет такого значения")
+// }
