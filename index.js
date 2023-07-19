1bae050fc8b773a1ffab68ae0171b4e1ffde7d85
Maxmud <maxmudov0696@mail.com>
2023-07-19 18:23:08 +0500

    lesson21

--- index.js
+++ index.js
@@ -49,3 +49,42 @@
 //     default :
 //     console.log ("нет такого значения")
 // }
+//  const hamburger = true; 
+//  const fries = true;
+//  if (hamburger && fries){
+//     console.log('Я сыт!');
+//  }
+
+
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+
