de981811952d96758979cd28f9a22ce45a6ea95b
Maxmud <maxmudov0696@mail.com>
2023-07-25 20:33:29 +0500

    if-else

--- index.js
+++ index.js
@@ -127,7 +127,82 @@
 //     }
      
 // }
+// var num =prompt ("Введите число столбцов");
+// let result = '';
+// for(let i=1 ; i<num ; i++){
+//     for (let j=0; j<i ; j++){
+//         result+= '*';
+//     }
+//     result+='\n';
+// }
+// console.log(result);
 
+// for(let i=20; i>=10 ; i--){
+//     if(i==13){
+//         break;
 
+// }
+//     console.log(i)
+// }
 
-    
+// for (let i=2; i<=10; i++){
+//     if(i%2==0){
+//        console.log(i)
+//     }
+   
+// }
+// var i=2;
+// while( i<=16) {
+//     if (i % 2 === 0) {
+//         i++;
+//     continue;
+// } else {
+//     console.log(i);
+// }
+//     i++;
+
+// }
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for(let i=0; i<arr.length;i++){
+//     result[i]=arr[i];
+// }
+// console.log(result);
+// return result;
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// for(var i=0; i<data.length; i++){
+//     if(typeof(data[i])==='number'){
+//         data[i] = data[i] * 2;
+//     }
+//     else if (typeof(data[i])=== "string") {
+//         data[i]=`${data[i]}-done`;
+//     }
+// }
+// console.log(data);
+// return data;
+
+/* let admin;
+let name;
+
+name= 'John';
+admin =name;
+
+alert ( admin ); */
+
+/* let ourPlanetName= "Yer";
+alert(Zemlya); */
+
+// Создайте переменную для хранения имени текущего посетителя сайта. Как бы вы назвали такую переменную?
+
+// let nameOfJs= prompt ("Название компании JS?")
+
+// if (nameOfJs=="ECMAScript"){
+//     alert ("Верно!");
+// }
+// else{
+//     alert ("Неверно");
+// }
